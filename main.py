5c308fd2c9ea418782e4e51755cb429014536e71
matttattoli@gmail.com <matttattoli@gmail.com>
2020-06-13 16:41:47 -0400

    only hook keys when modifier key is down

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ import keyboard
 
 
 class OneHandedKeyboard:
-
     def __init__(self):
         with open('remaps.json', 'r') as self.remaps:
             self.config = json.load(self.remaps)
         self.modifier_key = self.config['modifier']
         self.modifying = False
         self.remap_keys = self.config['key_maps']
-        self.key_states = {"down": True, "up": False}
-        self.hook_keys()
+        self.unhook_keys()
         keyboard.wait()
 
     def modifier_callback(self, kb_event):
         if kb_event.name == self.modifier_key:
-            self.modifying = self.key_states[kb_event.event_type]
+            if self.modifying and kb_event.event_type == 'up':
+                self.unhook_keys()
+                self.modifying = False
+            elif not self.modifying and kb_event.event_type == 'down':
+                self.hook_keys()
+                self.modifying = True
 
     def key_callback(self, kb_event):
         if keyboard.is_pressed('shift'):
@@ -35,6 +38,9 @@ class OneHandedKeyboard:
     def hook_keys(self):
         for key in self.remap_keys.keys():
             keyboard.hook_key(key, self.key_callback, suppress=True)
+
+    def unhook_keys(self):
+        keyboard.unhook_all()
         keyboard.hook_key(self.modifier_key,
                           self.modifier_callback,
                           suppress=True)
