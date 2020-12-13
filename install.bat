440742af2cf1154fbc21b6b412ca508d4285dde3
Matthew Tattoli <matttattoli@gmail.com>
2020-12-13 16:45:23 -0500

    add pip install

--- install.bat
+++ install.bat
@@ -3,5 +3,14 @@
 echo Copying main.py to "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 copy main.py "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\onehandedtyping.pyw"
 
+echo.
+echo.
+
 echo Copying ohtremaps.json to "%USERPROFILE%\.ohtremaps.json"
 copy ohtremaps.json "%USERPROFILE%\.ohtremaps.json"
+
+echo.
+echo.
+
+echo Installing pip packages
+pip3 install -r requirements.txt
